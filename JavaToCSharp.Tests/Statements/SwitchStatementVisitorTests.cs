e8df637c1caa6fd599a4a3d6b62a83769424d961
sangxxh <sanghan.dev@protonmail.com>
2021-09-25 02:01:03 +1000

    Handle missing break in switch statement

---
+++ JavaToCSharp.Tests/Statements/SwitchStatementVisitorTests.cs
@@ -0,0 +1,15 @@
+﻿using JavaToCSharp;
+using Xunit;
+
+namespace JavaToCSharp.Tests.Statements
+{
+    public class SwitchStatementVisitorTests
+    {
+        [Fact]
+        public void Test()
+        {
+            var context = new ConversionContext(new JavaConversionOptions());
+
+        }
+    }
+}
