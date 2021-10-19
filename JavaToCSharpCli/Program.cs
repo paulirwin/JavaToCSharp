bb354f65aa31811ee398ff3eaa5a47b4c0580ea8
Tom Tang <tom.tang@doway.com.tw>
2021-10-20 01:52:11 +0800

    New feature, batch mode command for cli.
    **First argument becomes a mode indicator.**
    
    - f mode: file to file convert.
    - d mode: folder to folder convert -> means get ting all *.java files for converting to *.cs.

--- JavaToCSharpCli/Program.cs
+++ JavaToCSharpCli/Program.cs
@@ -8,28 +8,61 @@ namespace JavaToCSharpCli
     {
         public static void Main(string[] args)
         {
-            if (args == null || args.Length < 2)
-            {
-                Console.WriteLine("Usage: JavaToCSharpCli.exe [pathToJavaFile] [pathToCsOutputFile]");
-                return;
-            }
+            if (args == null || args.Length < 3)
+                ShowHelp();
+            else
+                switch (args[0])
+                {
+                    case "-f":
+                        ConvertToCSharpFile(args[1], args[2]);
+                        break;
+                    case "-d":
+                        ConvertToCSharpDir(args[1], args[2]);
+                        break;
+                    default:
+                        ShowHelp();
+                        break;
+                }
+        }
 
-            if (!File.Exists(args[0]))
+        private static void ConvertToCSharpDir(string folderPath, string outputFolderPath)
+        {
+            if (Directory.Exists(folderPath))
             {
+                var input = new DirectoryInfo(folderPath);
+                var output = new DirectoryInfo(outputFolderPath);
+                foreach (var f in input.GetFiles("*.java", SearchOption.AllDirectories))
+                    ConvertToCSharpFile(
+                        f.FullName,
+                        Path.Combine(output.FullName, f.Name.Replace(f.Extension, ".cs")));
+            }
+            else
+                Console.WriteLine("Java input folder doesn't exist!");
+        }
+
+        private static void ConvertToCSharpFile(string filePath, string outputFilePath)
+        {
+            if (File.Exists(filePath))
+            {
+                var javaText = File.ReadAllText(filePath);
+                var options = new JavaConversionOptions();
+
+                options.WarningEncountered += (sender, eventArgs)
+                    => Console.WriteLine($"[WARNING] Line {eventArgs.JavaLineNumber}: {eventArgs.Message}");
+
+                var parsed = JavaToCSharpConverter.ConvertText(javaText, options);
+
+                File.WriteAllText(outputFilePath, parsed);
+                Console.WriteLine("Done!");
+            }
+            else
                 Console.WriteLine("Java input file doesn't exist!");
-                return;
-            }
+        }
 
-            var javaText = File.ReadAllText(args[0]);
-
-            var options = new JavaConversionOptions();
-
-            options.WarningEncountered += (sender, eventArgs) => Console.WriteLine($"[WARNING] Line {eventArgs.JavaLineNumber}: {eventArgs.Message}");
-
-            var parsed = JavaToCSharpConverter.ConvertText(javaText, options);
-
-            File.WriteAllText(args[1], parsed);
-            Console.WriteLine("Done!");
+        private static void ShowHelp()
+        {
+            Console.WriteLine("Usage:\r\n\tJavaToCSharpCli.exe -f [pathToJavaFile] [pathToCsOutputFile]");
+            Console.WriteLine("Usage:\tJavaToCSharpCli.exe -d [pathToJavaFolder] [pathToCsOutputFolder]");
         }
     }
 }
