afc010bc6e40dd13712a07b3a4b8a076a84cb77d
Paul Irwin <paulirwin@gmail.com>
2023-08-26 13:02:17 -0600

    WIP on adding System.Commandline to the CLI, #58

--- JavaToCSharpCli/Program.cs
+++ JavaToCSharpCli/Program.cs
@@ -1,111 +1,124 @@
-﻿using JavaToCSharp;
+﻿using System.CommandLine;
+using JavaToCSharp;
 using Microsoft.Extensions.Logging;
 
 namespace JavaToCSharpCli;
 
+/// <summary>
+/// The main JavaToCSharpCli program class.
+/// </summary>
 public class Program
 {
+    private static readonly ILoggerFactory _loggerFactory;
     private static readonly ILogger _logger;
 
     static Program()
     {
-        var loggerFactory =
-            LoggerFactory.Create(builder => builder.AddSimpleConsole().SetMinimumLevel(LogLevel.Information));
-        _logger = loggerFactory.CreateLogger<Program>();
+        _loggerFactory = LoggerFactory.Create(builder =>
+            builder.AddSimpleConsole().SetMinimumLevel(LogLevel.Information));
+        _logger = _loggerFactory.CreateLogger<Program>();
     }
 
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        try
-        {
-            if (args.Length < 3)
-                ShowHelp();
-            else
-                switch (args[0])
+        var inputOption = new Option<FileSystemInfo?>(
+            name: "--input",
+            description: "A Java source code file or directory to convert");
+
+        var outputOption = new Option<FileSystemInfo?>(
+            name: "--output",
+            description: "Path to a file or directory for the C# output");
+
+        var rootCommand = new RootCommand("Java to C# Converter");
+        rootCommand.AddOption(inputOption);
+        rootCommand.AddOption(outputOption);
+
+        rootCommand.SetHandler((input, output) =>
+            {
+                if (input is FileInfo inputFile)
                 {
-                    case "-f":
-                        ConvertToCSharpFile(args[1], args[2]);
-                        break;
-                    case "-d":
-                        ConvertToCSharpDir(args[1], args[2]);
-                        break;
-                    default:
-                        ShowHelp();
-                        break;
-                }
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Error: {Message}", ex.Message);
-        }
+                    if (output is not FileInfo outputFile)
+                    {
+                        _logger.LogError("Expected a file, not a directory, as output");
+                        return;
+                    }
 
-        // allow logger background thread to flush
-        Thread.Sleep(TimeSpan.FromSeconds(1));
+                    ConvertToCSharpFile(inputFile, outputFile);
+                }
+                else if (input is DirectoryInfo inputDirectory)
+                {
+                    if (output is not DirectoryInfo outputDirectory)
+                    {
+                        _logger.LogError("Expected a directory, not a file, as output");
+                        return;
+                    }
+
+                    ConvertToCSharpDir(inputDirectory, outputDirectory);
+                }
+            },
+            inputOption, outputOption);
+
+        await rootCommand.InvokeAsync(args);
+
+        // flush logs
+        _loggerFactory.Dispose();
     }
 
-    private static void ConvertToCSharpDir(string folderPath, string outputFolderPath)
+    private static void ConvertToCSharpDir(DirectoryInfo inputDirectory, DirectoryInfo outputDirectory)
     {
-        if (Directory.Exists(folderPath))
+        if (inputDirectory.Exists)
         {
-            var input = new DirectoryInfo(folderPath);
-            foreach (var f in input.GetFiles("*.java", SearchOption.AllDirectories))
+            foreach (var f in inputDirectory.GetFiles("*.java", SearchOption.AllDirectories))
             {
                 string? directoryName = f.DirectoryName;
                 if (string.IsNullOrWhiteSpace(directoryName))
                 {
                     continue;
                 }
-                
-                string newFolderPath = directoryName.Replace(folderPath, outputFolderPath, StringComparison.OrdinalIgnoreCase);
-                if (!Directory.Exists(newFolderPath))
+
+                if (!outputDirectory.Exists)
                 {
-                    Directory.CreateDirectory(newFolderPath);
+                    outputDirectory.Create();
                 }
 
-                ConvertToCSharpFile(f.FullName,
-                                    Path.Combine(newFolderPath, Path.ChangeExtension(f.Name, ".cs")),
-                                    false);
+                ConvertToCSharpFile(f,
+                    new FileInfo(Path.Combine(outputDirectory.FullName, Path.ChangeExtension(f.Name, ".cs"))),
+                    false);
             }
         }
         else
-            _logger.LogError("Java input folder {path} doesn't exist!", folderPath);
+            _logger.LogError("Java input folder {path} doesn't exist!", inputDirectory);
     }
 
-    private static void ConvertToCSharpFile(string filePath, string outputFilePath, bool overwrite = true)
+    private static void ConvertToCSharpFile(FileSystemInfo inputFile, FileSystemInfo outputFile, bool overwrite = true)
     {
-        if (!overwrite && File.Exists(outputFilePath))
-            _logger.LogInformation("{outputFilePath} exists, skip to next.", outputFilePath);
-        else if (File.Exists(filePath))
+        if (!overwrite && outputFile.Exists)
+            _logger.LogInformation("{outputFilePath} exists, skip to next.", outputFile);
+        else if (inputFile.Exists)
         {
             try
             {
-                string javaText = File.ReadAllText(filePath);
+                string javaText = File.ReadAllText(inputFile.FullName);
                 var options = new JavaConversionOptions();
 
                 options.WarningEncountered += (_, eventArgs) =>
-                                              {
-                                                  _logger.LogWarning("Line {JavaLineNumber}: {Message}", eventArgs.JavaLineNumber, eventArgs.Message);
-                                                  File.AppendAllText(Path.ChangeExtension(outputFilePath, ".warning"), eventArgs.Message + Environment.NewLine);
-                                              };
+                {
+                    _logger.LogWarning("Line {JavaLineNumber}: {Message}", eventArgs.JavaLineNumber, eventArgs.Message);
+                    File.AppendAllText(Path.ChangeExtension(outputFile.FullName, ".warning"),
+                        eventArgs.Message + Environment.NewLine);
+                };
 
                 string? parsed = JavaToCSharpConverter.ConvertText(javaText, options);
-                File.WriteAllText(outputFilePath, parsed);
-                _logger.LogInformation("{filePath} converted!", filePath);
+                File.WriteAllText(outputFile.FullName, parsed);
+                _logger.LogInformation("{filePath} converted!", inputFile.Name);
             }
             catch (Exception ex)
             {
-                _logger.LogError("{filePath} failed! {type}: {message}", filePath, ex.GetType().Name, ex);
-                File.WriteAllText(Path.ChangeExtension(outputFilePath, ".error"), ex.ToString());
+                _logger.LogError("{filePath} failed! {type}: {message}", inputFile.Name, ex.GetType().Name, ex);
+                File.WriteAllText(Path.ChangeExtension(outputFile.FullName, ".error"), ex.ToString());
             }
         }
         else
-            _logger.LogError("Java input file {filePath} doesn't exist!", filePath);
-    }
-
-    private static void ShowHelp()
-    {
-        Console.WriteLine("Usage:");
-        Console.WriteLine("\tJavaToCSharpCli.exe -f [pathToJavaFile] [pathToCsOutputFile]");
-        Console.WriteLine("\tJavaToCSharpCli.exe -d [pathToJavaFolder] [pathToCsOutputFolder]");
+            _logger.LogError("Java input file {filePath} doesn't exist!", inputFile.FullName);
     }
 }
